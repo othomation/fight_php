f4323d2cc2459a41f19f4f0829ea20b976708376
otho <contact@otho.bike>
2021-06-21 13:53:00 +0200

    (refactoring) less code in index

---
+++ bin/init.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once './models/Game.php';
+require_once './models/Player/Player.php';
+
+$playerOne;
+$playerTwo;
+
+if (Helpers::validatePost($_POST)) {
+	$playerOne = new Player($_POST["name"], floatval($_POST["hp"]), floatval($_POST["dmg"]), $_POST["race"], new Shouts($_POST["shoutVictory"], $_POST["shoutHit"]));
+	$playerTwo = new Player($_POST["name1"], floatval($_POST["hp1"]), floatval($_POST["dmg1"]), $_POST["race1"], new Shouts($_POST["shoutVictory1"], $_POST["shoutHit1"]));
+} else {
+	$playerOne = new Player("DagDag", 60, 5, 'orc', new Shouts('Bien fait pour toi !', 'Ouuuugaaaaahhhh !!!'));
+	$playerTwo = new Player("Elys", 45, 11, 'elf', new Shouts('Paix pour la nature.', 'Par les forces de la nature !'));
+}
+
+$game = new Game(new PlayerRepository($playerOne, $playerTwo), new ComponentsRepository());
--- round.php
+++ round.php
@@ -1,40 +1,37 @@
-<link rel="stylesheet" href="./assets/styles.css">
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-require_once './models/Game.php';
-require_once './models/Player/Player.php';
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./assets/styles.css">
+	<title>Fight !</title>
+</head>
 
-$playerOne;
-$playerTwo;
+<body>
+	<?php require_once './bin/init.php'; ?>
 
-if (Helpers::validatePost($_POST)) {
-	$playerOne = new Player($_POST["name"], floatval($_POST["hp"]), floatval($_POST["dmg"]), $_POST["race"], new Shouts($_POST["shoutVictory"], $_POST["shoutHit"]));
-	$playerTwo = new Player($_POST["name1"], floatval($_POST["hp1"]), floatval($_POST["dmg1"]), $_POST["race1"], new Shouts($_POST["shoutVictory1"], $_POST["shoutHit1"]));
-} else {
-	$playerOne = new Player("DagDag", 60, 5, 'orc', new Shouts('Bien fait pour toi !', 'Ouuuugaaaaahhhh !!!'));
-	$playerTwo = new Player("Elys", 45, 11, 'elf', new Shouts('Paix pour la nature.', 'Par les forces de la nature !'));
-}
+	<section id="players">
+		<?= Helpers::getPlayerCard($playerOne, 'one'); ?>
+		<?= Helpers::getPlayerCard($playerTwo, 'two'); ?>
+	</section>
 
-$game = new Game(new PlayerRepository($playerOne, $playerTwo), new ComponentsRepository());
+	<!-- START THE GAME ! -->
+	<?php $game->start(); ?>
+	<!-- GAME ENDED ! -->
 
+	<main>
+		<section id="rounds">
+			<?php foreach ($game->componentQueue->getRounds() as $component) : ?>
+				<?= $component ?>
+			<?php endforeach; ?>
+		</section>
 
-?>
+		<section id="result">
+			<?= $game->componentQueue->getResult() ?>
+		</section>
+	</main>
+</body>
 
-<section id="players">
-	<?= Helpers::getPlayerCard($playerOne, 'one'); ?>
-	<?= Helpers::getPlayerCard($playerTwo, 'two'); ?>
-</section>
-
-<!-- START THE GAME ! -->
-<?php $game->start(); ?>
-<!-- GAME ENDED ! -->
-
-<section id="rounds">
-	<?php foreach ($game->componentQueue->getRounds() as $component) : ?>
-		<?= $component ?>
-	<?php endforeach; ?>
-</section>
-
-<section id="result">
-	<?= $game->componentQueue->getResult() ?>
-</section>
+</html>
